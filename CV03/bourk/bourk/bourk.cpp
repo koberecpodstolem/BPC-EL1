ba55a980c2c160cdfe2d1c2e5a394e2e2567734d
Student <student@sa305>
2024-10-01 17:30:39 +0200

    created new project

---
+++ CV03/bourk/bourk/bourk.cpp
@@ -0,0 +1,19 @@
+﻿// bourk.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "bourk.h"
+
+using namespace std;
+
+int main()
+{
+	int speed_of_sound = 340;
+	int time;
+	printf("Insert time in  seconds: ");
+	cin >> time;
+	int distance = speed_of_sound * time;
+	printf("The storm is ");
+	printf("%d", distance);
+	printf(" meters away");
+	return 0;
+}
