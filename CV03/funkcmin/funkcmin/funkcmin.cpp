ac1c860fec0e8233719d61cfeb21fed0e30c7c87
Student <student@sa305>
2024-10-01 17:39:48 +0200

    Create funcmax

---
+++ CV03/funkcmin/funkcmin/funkcmin.cpp
@@ -0,0 +1,24 @@
+﻿// funkcmin.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "funkcmin.h"
+
+using namespace std;
+
+int main()
+{
+    double maxY = DBL_MIN;
+    int maxX = INT_MIN;
+    for (int x = 10; x <= 20; ++x)
+    {
+        double y = 5 - 3 * x + 2 * (x - 5) * (x - 5) - (x - 10) * (x - 10) * (x - 10);
+
+        if (y > maxY)
+        {
+            maxY = y;
+            maxX = x;
+        }
+    }
+    printf("Maximum value: %lf at x=%d\n", maxY, maxX);
+    return 0;
+}
