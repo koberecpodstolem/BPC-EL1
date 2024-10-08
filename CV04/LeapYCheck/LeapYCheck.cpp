f9592fde0cf888d629b64f85c86ebbee8e405556
Matěj Hromek <270939@vutbr.cz>
2024-10-08 17:16:57 +0200

    CV04 commit

---
+++ CV04/LeapYCheck/LeapYCheck.cpp
@@ -0,0 +1,28 @@
+﻿// LeapYCheck.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "LeapYCheck.h"
+#include "isLeap.h"
+
+using namespace std;
+
+int main()
+{
+	printf("1000   ");
+	printf("%d", isLeap(1000));
+
+	printf("\n2000   ");
+	printf("%d", isLeap(2000));
+
+	printf("\n2002   ");
+	printf("%d", isLeap(2002));
+
+	printf("\n2012   ");
+	printf("%d", isLeap(2012));
+
+	printf("\n2022   ");
+	printf("%d", isLeap(2022));
+
+	printf("\n2200   ");
+	printf("%d", isLeap(2200));
+}
